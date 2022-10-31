6ec889f36dedff329f9a023a2ff6abb403cad7c5
DanielLien <Daniellien2018@gmail.com>
2022-10-31 15:08:39 -0700

    added Resizing Int Set

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -36,18 +36,25 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+    end
   end
 
   def include?(num)
+   return true if self[num].include?(num)
+   false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 
   def num_buckets
@@ -64,18 +71,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num) 
+  
+    @count +=1
+    resize! if count >= @store.length
+    self[num] << num
+    true
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1 
+    end
   end
 
   def include?(num)
+   return true if self[num].include?(num)
+   false
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 
   def num_buckets
@@ -83,5 +102,14 @@ class ResizingIntSet
   end
 
   def resize!
+    if count == @store.length
+      old = @store.map do |bucket|
+        bucket
+      end
+      @store = Array.new(count * 2) {Array.new(0)}
+      old.each do |bucket|
+        bucket.each {|val| self[val % @store.length] << val }
+      end
+    end
   end
 end
